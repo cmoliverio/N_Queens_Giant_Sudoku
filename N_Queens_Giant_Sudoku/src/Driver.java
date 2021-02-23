8a78fe9b169bca7628fcdeae0547738171625cbb
cmoliverio <cmoliverio.10@gmail.com>
2021-02-23 12:36:10 -0500

    Initial commit, contains eclipse project.

---
+++ N_Queens_Giant_Sudoku/src/Driver.java
@@ -0,0 +1,14 @@
+/**
+ * @author Christian Oliverio, Jack Peterson, Andrew Kivrak, Benjamin Bowering
+ * @date 23 February 2021
+ * @project Project 2
+ * @description Project fits n number of queens into an n x n grid
+ * or solves a sudoku of size 25 x 25
+ */
+
+
+public class Driver {
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+	}
+}
